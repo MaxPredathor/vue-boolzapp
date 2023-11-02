5b5cabe739cbab288b39c6b50c494f85326136e3
MaxPredathor <max.gilli98@gmail.com>
2023-11-02 19:22:38 +0100

    MILESTONE 3 COMPLETE

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-const { createApp } = Vue;
 import { contactsList } from "./data.js";
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
       contacts: contactsList,
       activeId: 1,
+      newMessage: "",
     };
   },
-  methods: {},
+  methods: {
+    addMessage() {
+      this.contacts[this.activeContact].messages.push({
+        date: new Date().toLocaleString(),
+        message: this.newMessage,
+        status: "sent",
+      });
+      this.newMessage = "";
+      setTimeout(() => {
+        this.addAnswer();
+      }, 1000);
+    },
+    addAnswer() {
+      this.contacts[this.activeContact].messages.push({
+        date: new Date().toLocaleString(),
+        message: "Ok",
+        status: "received",
+      });
+    },
+  },
   computed: {
     activeContact() {
       return this.contacts.findIndex((contact) => contact.id === this.activeId);
